3006f819b87dd239dd5b47f230ce015f5567e229
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2023-09-24 19:30:09 +0200

    There's honestly too many changes to name

---
+++ Assets/_Project/Runtime/Scripts/StateMachine/State Scripts/MoveState.cs
@@ -0,0 +1,68 @@
+﻿#region
+using UnityEngine;
+#endregion
+
+//TODO: If you hold move, then exit the state, upon entering the state you wont move until you release and repress the move button.
+public class MoveState : State
+{
+    float moveSpeed;
+
+    public override StateType Type => StateType.Walk;
+    public override int Priority => statePriorities[Type];
+
+    public bool IsMoving { get; private set; }
+
+    public MoveState(PlayerController player, MoveStateData stateData) : base(player)
+    {
+        moveSpeed = stateData.MoveSpeed; 
+    }
+
+    public override bool CanBeInterrupted()
+    {
+        // return true if the player is doing anything other than moving
+        return player.IsAttacking() || player.IsAirborne();
+    }
+
+    public override void OnEnter()
+    {
+        // Play the move animation.
+        // Log("Entered Walk State");
+        IsMoving = true;
+
+        player.GetComponentInChildren<SpriteRenderer>().color = Color.blue;
+    }
+
+    //MOVEMENT ISN'T MY PROBLEM HAHAHAHAHA HAVE FUN WITH THIS HEHEHE
+    public override void UpdateState()
+    {
+        // Handle move logic
+        Vector3 moveInput = player.InputManager.MoveInput;
+
+        if (moveInput.sqrMagnitude > 0.01)
+        {
+            // Apply horizontal and vertical movement
+            Vector3 movement = moveInput * (moveSpeed * Time.deltaTime);
+            player.Rigidbody.velocity = new (movement.x, player.Rigidbody.velocity.y, movement.z);
+        }
+        else { OnExit(); }
+    }
+
+    public override void OnExit()
+    {
+        // Perform any necessary cleanup or exit actions
+        //Debug.Log("Exited Move State");
+
+        // Slow down the player
+        Vector3 velocity = player.Rigidbody.velocity;
+        velocity                  = new (velocity.x * 0.9f, velocity.y, velocity.z);
+        player.Rigidbody.velocity = velocity;
+
+        // Pass control to the idle state
+        
+        // TODO: If we enter the Idle state whenever we stop moving, we'll gain a lot of speed when we start moving again as OnEnter is called.
+        //player.StateMachine.TransitionToState(StateType.Idle);
+        
+        // Reset the IsMoving flag
+        IsMoving = false;
+    }
+}
---
+++ Assets/_Project/Runtime/Scripts/Utility/PlayerDetails.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public partial class PlayerManager
+{
+    [Serializable]
+    public struct PlayerDetails
+    {
+        [Space(5)]
+        public TargetPlayer targetPlayer;
+        
+        [Space(10)]
+        public PlayerColor playerColors;
+        
+        [Space(10)]
+        public PlayerSpawnPoints playerSpawnPoints;
+        
+        [Space(10)]
+        public PlayerHealthbars playerHealthbars;
+    }
+
+    /// <summary> Player enum is used in a context where the <see cref="PlayerController.PlayerID"/> is needed. </summary>
+    [Flags]
+    public enum TargetPlayer
+    {
+        PlayerOne = 1,
+        PlayerTwo = 2,
+        [UsedImplicitly] // Used in the inspector through the "Everything" option.
+        Both = PlayerOne | PlayerTwo
+    }
+
+    [Serializable]
+    public struct PlayerColor
+    {
+        public Color playerOneColor;
+        public Color playerTwoColor;
+    }
+
+    [Serializable]
+    public struct PlayerSpawnPoints
+    {
+        public Vector2 playerOneSpawnPoint;
+        public Vector2 playerTwoSpawnPoint;
+    }
+
+    [Serializable] //TODO: Implement this when the healthbar is done.
+    public struct PlayerHealthbars
+    {
+        public Slider playerOneHealthbar;
+        public Slider playerTwoHealthbar;
+    }
+}
