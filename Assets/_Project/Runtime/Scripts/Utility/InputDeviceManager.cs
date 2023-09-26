80d0494522aa6ff1994bfbb100566cc6a8d0506d
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2023-09-26 14:32:54 +0200

    Update input methods to support co-op gameplay
    Updated the player input methods and added the InputDeviceManager script. These changes allow the game to support co-op gameplay by adding new control schemes for a second player and assigning each player's controls to specific devices. Also adjusted the references to these controls across the project to align with the updated configuration. Players can join or leave the game by pressing a defined key, allowing dynamic participation.

---
+++ Assets/_Project/Runtime/Scripts/Utility/InputDeviceManager.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputDeviceManager : MonoBehaviour
+{
+    PlayerInputManager inputManager;
+    Dictionary<int, InputDevice> playerDevices = new ();
+    List<string> controlSchemes = new () { "Keyboard", "(Co-op) Keyboard", "Gamepad" };
+
+    void Awake()
+    {
+        inputManager = FindObjectOfType<PlayerInputManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            OnJoinPlayer();
+        }
+    }
+
+    void OnJoinPlayer()
+    {
+        // For keyboard.
+        if (Keyboard.current.wasUpdatedThisFrame && inputManager.playerCount < 3)
+        {
+            InputDevice device = Keyboard.current;
+
+            int playerIndex = inputManager.playerCount;
+
+            if (playerIndex > 1)
+            {
+                if (playerDevices.ContainsValue(device)) return;
+            }
+
+            playerDevices[playerIndex] = device;
+
+            string controlScheme = controlSchemes[playerIndex];
+            
+            inputManager.JoinPlayer(playerIndex, -1, controlScheme, device);
+
+            Debug.Log($"Player {playerIndex} joined using {controlScheme}");
+        }
+        
+        // For Gamepad.
+        if (Gamepad.current == null) return;
+        {
+            if (!Gamepad.current.wasUpdatedThisFrame || inputManager.playerCount >= 2) return;
+            InputDevice device = Gamepad.current;
+
+            int playerIndex = inputManager.playerCount;
+
+            if (playerIndex > 1)
+            {
+                if (playerDevices.ContainsValue(device)) return;
+            }
+
+            playerDevices[playerIndex] = device;
+            
+            string controlScheme = controlSchemes[2]; // Will always be Gamepad.
+
+            inputManager.JoinPlayer(playerIndex, -1, controlScheme, device);
+
+            Debug.Log($"Player {playerIndex} joined using {controlScheme}");
+        }
+    }
+
+    public void OnPlayerLeave()
+    {
+        InputDevice device = Keyboard.current;
+
+        int playerIndex = inputManager.playerCount;
+
+        if (playerIndex > -1)
+        {
+            if (playerDevices.ContainsValue(device))
+            {
+                playerDevices.Remove(playerIndex);
+                Debug.Log($"Player {playerIndex} left");
+            }
+        }
+    }
+}
