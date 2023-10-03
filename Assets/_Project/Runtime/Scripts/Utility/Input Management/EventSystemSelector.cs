d04b0c9dfa9787026941c15329a4c65a6ce17881
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2023-10-03 14:11:33 +0200

    Fixed UI navigation in menus.

--- Assets/_Project/Runtime/Scripts/Utility/Input Management/EventSystemSelector.cs
+++ Assets/_Project/Runtime/Scripts/Utility/Input Management/EventSystemSelector.cs
@@ -19,7 +19,7 @@ public class EventSystemSelector : MonoBehaviour
     Button firstSelected;
 
     // In-case the serialized field is null, we can get the PlayerInput component from the parent.
-    void Awake() => playerInput = transform.parent.GetComponentInChildren<PlayerInput>();
+    void Awake() => playerInput = GetComponent<PlayerInput>();
 
     void OnEnable()
     {
