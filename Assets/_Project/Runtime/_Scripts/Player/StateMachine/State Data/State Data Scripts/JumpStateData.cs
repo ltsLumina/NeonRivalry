433f242b1156cb8586d1c7462da03af8f0b19cfd
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2023-10-09 21:07:10 +0200

    Moved thing around.
    Signed-off-by: Lumina <alexander.andrejeff@elev.ga.lbs.se>

--- Assets/_Project/Runtime/_Scripts/Player/StateMachine/State Data/State Data Scripts/JumpStateData.cs
+++ Assets/_Project/Runtime/_Scripts/Player/StateMachine/State Data/State Data Scripts/JumpStateData.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "JumpStateData", menuName = "State Data/JumpState Data", order = 2)]
 public class JumpStateData : DefaultStateData
 {
-    [SerializeField, Tooltip("The force applied to the player when jumping. \nDefault Value: 650")] 
-    float jumpForce;
-    [SerializeField, Tooltip("The duration of the jump. \nDefault Value: 0.1")]
-    float jumpDuration;
-    [SerializeField, Tooltip("The mass of the player while falling. \nDefault Value: 1.5")]
-    float playerMass;
+    [Tooltip("The force applied to the player when jumping. \nDefault Value: 650")]
+    [SerializeField] float jumpForce;
+    
+    [Tooltip("The duration of the jump. \nDefault Value: 0.1")]
+    [SerializeField] float jumpDuration;
+    
+    [Tooltip("The mass of the player while falling. \nDefault Value: 1.5")]
+    [SerializeField] float playerMass;
     
     public float JumpForce => jumpForce;
     public float JumpDuration => jumpDuration;
