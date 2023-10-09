5c25e8d79ab359e61938b51a7a4274e590af0e72
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2023-10-09 21:13:43 +0200

    moreeeeeeeee
    Signed-off-by: Lumina <alexander.andrejeff@elev.ga.lbs.se>

--- Assets/_Project/Runtime/_Scripts/Player/StateMachine/State Scripts/MoveState.cs
+++ Assets/_Project/Runtime/_Scripts/Player/StateMachine/State Scripts/MoveState.cs
@@ -2,7 +2,6 @@
 using UnityEngine;
 #endregion
 
-//TODO: If you hold move, then exit the state, upon entering the state you wont move until you release and repress the move button.
 public class MoveState : State
 {
     float moveSpeed;
@@ -23,37 +22,42 @@ public class MoveState : State
         velocityPower = stateData.AccelerationRate;
     }
 
-    public override bool CanBeInterrupted()
-    {
+    public override bool CanBeInterrupted() =>
         // return true if the player is doing anything other than moving
-        return player.IsAttacking() || player.IsAirborne();
-    }
+        player.IsAttacking() || player.IsAirborne();
 
     public override void OnEnter()
     {
         // Play the move animation.
-        // Log("Entered Walk State");
         IsMoving = true;
 
         player.GetComponentInChildren<SpriteRenderer>().color = Color.blue;
     }
-    
+
     public override void UpdateState()
     {
-        // Handle move logic
+        // Getting the move input from the player's input manager.
         Vector3 moveInput = player.InputManager.MoveInput;
-        int moveDirection = (int)moveInput.x;
 
+        // Determining the direction of the movement (left or right).
+        int moveDirection = (int) moveInput.x;
+
+        // Calculating the target speed based on direction and move speed.
         float targetSpeed = moveDirection * moveSpeed;
 
+        // Calculate difference between target speed and current velocity.
         float speedDifference = targetSpeed - player.Rigidbody.velocity.x;
 
+        // Determine the acceleration rate based on whether the target speed is greater than 0.01 or not.
         float accelRate = Mathf.Abs(targetSpeed) > 0.01f ? acceleration : deceleration;
 
+        // Calculate the final movement force to be applied on the player's rigidbody.
         float movement = Mathf.Pow(Mathf.Abs(speedDifference) * accelRate, velocityPower) * Mathf.Sign(speedDifference);
 
+        // Apply the force to the player's rigidbody.
         player.Rigidbody.AddForce(movement * Vector3.right);
 
+        // Call the OnExit function after the force has been applied.
         OnExit();
     }
 
