14d88842e50e9a0be1515b245a765f18d6d0087a
Ministar200 <128044449+Ministar200@users.noreply.github.com>
2023-11-23 15:37:12 +0100

    Animated the performance bars

--- Assets/_Project/Runtime/_Scripts/UI/ResultScreen.cs
+++ Assets/_Project/Runtime/_Scripts/UI/ResultScreen.cs
@@ -1,34 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ResultScreen : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI winner;
+    [SerializeField] TextMeshProUGUI winner;  
+    // Get all sliders in the children of this GameObject
+    Slider[] sliders;
+    public float animationDuration = 2f;
+    public float targetValue = 1f;
+
+    private void Awake()
+    {
+        sliders = GetComponentsInChildren<Slider>();
+
+    }
 
     private void Start()
     {
-        RandomizeSliderValues(1, 100);
-        
+        StartCoroutine(RandomizeSliderValues(1, 100));
 
         FindObjectOfType<RoundManager>().player1Victory = true;
         if (FindObjectOfType<RoundManager>() == null) { return; }
         winner.text = FindObjectOfType<RoundManager>().player1Victory ? "Player 1 Has Won" : "Player 2 Has Won"; 
     }
 
-    // Function to randomize values for all sliders
-    public void RandomizeSliderValues(float minValue, float maxValue)
+    IEnumerator RandomizeSliderValues(float minValue, float maxValue)
     {
-        // Get all sliders in the children of this GameObject
-        Slider[] sliders = GetComponentsInChildren<Slider>();
-
         foreach (Slider slider in sliders)
         {
             // Generate a random value within the specified range
             float randomValue = Random.Range(minValue, maxValue) + Random.Range(1, 10) * Random.Range(0.05f, 2) / Random.Range(0.5f, 3);
-            slider.value = randomValue;
+
+            // Start the animation for each slider individually
+            StartCoroutine(AnimateSlider(slider, randomValue));
         }
+
+        yield return null;
+    }
+
+    IEnumerator AnimateSlider(Slider slider, float targetValue)
+    {
+        float startTime = Time.time;
+        float startValue = slider.value;
+
+        while (Time.time - startTime < animationDuration)
+        {
+            float progress = (Time.time - startTime  / animationDuration);
+            slider.value = Mathf.Lerp(startValue, targetValue, progress );
+            yield return null;
+        }
+
+        // Ensure the final value is exactly the target value
+        slider.value = targetValue;
     }
 }
