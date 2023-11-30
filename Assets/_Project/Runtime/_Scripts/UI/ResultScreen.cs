d303082c986bcbee49807df35149df4890518ee5
Ministar200 <128044449+Ministar200@users.noreply.github.com>
2023-11-30 14:25:35 +0100

    Slider filling is finished now

--- Assets/_Project/Runtime/_Scripts/UI/ResultScreen.cs
+++ Assets/_Project/Runtime/_Scripts/UI/ResultScreen.cs
@@ -23,6 +23,7 @@ public class ResultScreen : MonoBehaviour
 
         FindObjectOfType<RoundManager>().player1Victory = true;
         winner.text = FindObjectOfType<RoundManager>().player1Victory ? "Player 1 Has Won" : "Player 2 Has Won";
+
     }
 
     IEnumerator RandomizeSliderValues(float minValue, float maxValue)
@@ -32,26 +33,10 @@ public class ResultScreen : MonoBehaviour
             // Generate a random value within the specified range
             float randomValue = Random.Range(minValue, maxValue) + Random.Range(1, 10) * Random.Range(0.05f, 2) / Random.Range(0.5f, 3);
 
-            // Start the animation for each slider individually
-            StartCoroutine(AnimateSlider(slider, randomValue));
+            // Lerp the slider to the random value. The bool makes the slider fill in differently, I like true more.
+            slider.DOValue(randomValue, animationDuration, true);
         }
 
         yield return null;
-    }
-
-    IEnumerator AnimateSlider(Slider slider, float targetValue)
-    {
-        float startTime = Time.time;
-        float startValue = slider.value;
-
-        while (Time.time - startTime < animationDuration)
-        {
-            float progress = Time.time - startTime / animationDuration;
-            slider.value = Mathf.Lerp(startValue, targetValue, progress);
-            yield return null;
-        }
-
-        // Ensure the final value is exactly the target value
-        slider.value = targetValue;
     }
 }
