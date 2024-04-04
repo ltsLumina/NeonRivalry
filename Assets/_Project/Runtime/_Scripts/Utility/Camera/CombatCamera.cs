9d00bf8f94a18ec489de357f9f9f00f9d6ddc589
Ministar200 <128044449+Ministar200@users.noreply.github.com>
2024-04-04 12:54:34 +0200

    Camera finished (no y?)

--- Assets/_Project/Runtime/_Scripts/Utility/Camera/CombatCamera.cs
+++ Assets/_Project/Runtime/_Scripts/Utility/Camera/CombatCamera.cs
@@ -1,38 +1,30 @@
 using Cinemachine;
 using UnityEngine;
 
-public class VCamScriptName : MonoBehaviour
+public class CombatCamera : MonoBehaviour
 {
     [SerializeField] Transform target1;
     [SerializeField] Transform target2;
-    [SerializeField] Transform followTarget;
     public float minDistance = 5f; // Minimum distance between the players
     public float maxDistance = 10f; // Maximum distance between the players
     public float minZPosition = -10f; // Minimum z position of the camera
     public float maxZPosition = -5f; // Maximum z position of the camera
     public float zoomSpeed = 5f; // Zoom speed
-    public float borderPadding = 1f; // Padding to add around the camera's view
     CinemachineVirtualCamera vCam;
-    BoxCollider borderCollider;
-    Vector3 startPosition;
-    //Vector3 midpoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position;
         vCam = GetComponent<CinemachineVirtualCamera>();
-        borderCollider = GetComponent<BoxCollider>();
-        //vCam.m_Follow = followTarget;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Temp();
+        CenterandZoom();
     }
 
-    void Temp()
+    void CenterandZoom()
     {
         // Calculate the distance between the two players
         float distance = Vector3.Distance(target1.position, target2.position);
@@ -40,7 +32,7 @@ public class VCamScriptName : MonoBehaviour
         // Calculate the desired z-position based on the distance
         float desiredZ = Mathf.Lerp(maxZPosition, minZPosition, Mathf.InverseLerp(minDistance, maxDistance, distance));
 
-        // Smoothly adjust the z-position of the Virtual Camera
+        // Smoothly adjust the x- and z-position of the Virtual Camera
         Vector3 currentPosition = vCam.transform.position;
         Vector3 midpoint = (target1.position + target2.position) / 2;
         float newZ = Mathf.Lerp(currentPosition.z, desiredZ, Time.deltaTime * zoomSpeed);
